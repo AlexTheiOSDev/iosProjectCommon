86f172fd08562e8d23df6df2533ac60842f7d6da
Maksym Dyomin <maksym.dyomin@gmail.com>
2016-12-15 13:40:03 +0200

    Initial Commit

---
+++ iosProjectCommon/iosProjectCommon.h
@@ -0,0 +1,19 @@
+//
+//  iosProjectCommon.h
+//  iosProjectCommon
+//
+//  Created by Maksym Demin on 12/15/16.
+//  Copyright © 2016 Maksym Demin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iosProjectCommon.
+FOUNDATION_EXPORT double iosProjectCommonVersionNumber;
+
+//! Project version string for iosProjectCommon.
+FOUNDATION_EXPORT const unsigned char iosProjectCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iosProjectCommon/PublicHeader.h>
+
+
